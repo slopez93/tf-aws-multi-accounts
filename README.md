ef4a0c7f1473a86cb56030c36fdbca7fe61111b6
Santi Lopez <santilopez@Santis-MacBook-Pro.local>
2024-02-14 18:51:58 +0100

    tf inital

---
+++ README.md
@@ -0,0 +1,2 @@
+# TF AWS Multiaccount
+
---
+++ main.tf
@@ -0,0 +1,33 @@
+
+provider "aws" {
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::600175187333:role/terraform-admin-role"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+module "ec2-instance" {
+  source = "./modules/aws-ec2-instance"
+
+  ami_id        = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  instance_name = "My new ec2 instance"
+}
+
